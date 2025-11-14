fd67ff5ed766bbae1ad50d39f748c23ec19efe13
Nikolas <pegiosnikolas@gmail.com>
2025-11-14 02:19:53 +0200

    test4

--- backend/src/main/java/com/cinematch/CineMatch.java
+++ backend/src/main/java/com/cinematch/CineMatch.java
@@ -10,7 +10,10 @@ public class CineMatchApplication {
         SpringApplication.run(CineMatchApplication.class, args);
         System.out.println("\n🎬 CineMatch backend is now running...");
         System.out.println("test1\n");
+
         System.out.println("test2\n");
+        System.out.println("test3\n");
+
 
 
 
